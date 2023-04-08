db0bce0be05485dd3fbed1f34995832b38a8570a
Sung <l975464@naver.com>
2023-04-08 18:36:56 +0900

    모바일 반응형 폰트수정

--- public/javascripts/guide.js
+++ public/javascripts/guide.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
 
+    // Top 스크롤 버튼
+    $("#btn_top").click(function () {
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
 
+
+    // Down 스크롤 버튼
+    $("#btn_down").click(function () {
+        $('html, body').animate({
+            scrollTop: $("footer").offset().top
+        }, 500);
+    });
 });
